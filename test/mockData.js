52f6a8643ccd88045c181428d7044c51c2707369
Elena <karengvcs@gmail.com>
2020-05-11 06:16:02 -0500

    data para correr los test

--- test/mockData.js
+++ test/mockData.js
@@ -1,66 +1,254 @@
+/* eslint linebreak-style: ["error", "windows"] */
+// links from direc/file.md
 const links = [
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://atom.io/',
+    text: 'Atom',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://code.visualstudio.com/',
+    text: 'Code',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/tree/master/topics/shell',
+    text: 'UNIX Shell',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/e/master/topics/scm/01-git',
+    text: 'git',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://git-scm.com/download/win',
+    text: 'Git bash',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://help.github.com/articles/fork-a-repo/src',
+    text: 'fork',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://help.github.com/articles/cloning-a-repository/',
+    text: 'Clona',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
     href: 'https://nodejs.org/',
     text: 'Node.js',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://nodejs.org/es/',
-    text: 'Node.js',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://developers.google.com/v8/',
-    text: 'motor de JavaScript V8 de Chrome',
-  },
-  {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://nodejs.org/es/ths',
-    text: 'Link',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
   },
 ];
 
 const linksValidated = [
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://atom.io/',
+    text: 'Atom',
     status: 200,
     statusText: 'Ok',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://code.visualstudio.com/',
+    text: 'Code',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/tree/master/topics/shell',
+    text: 'UNIX Shell',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/e/master/topics/scm/01-git',
+    text: 'git',
+    status: 404,
+    statusText: 'Fail',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://git-scm.com/download/win',
+    text: 'Git bash',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://help.github.com/articles/fork-a-repo/src',
+    text: 'fork',
+    status: 404,
+    statusText: 'Fail',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://help.github.com/articles/cloning-a-repository/',
+    text: 'Clona',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
     href: 'https://nodejs.org/',
     text: 'Node.js',
     status: 200,
     statusText: 'Ok',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://nodejs.org/es/',
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: 'C:\\Users\\karen\\Documents\\laboratoria\\LIM012-fe-md-links\\test\\direc\\file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
+    status: 200,
+    statusText: 'Ok',
+  },
+];
+
+/* const links = [
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://atom.io/',
+    text: 'Atom',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://code.visualstudio.com/',
+    text: 'Code',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/tree/master/topics/shell',
+    text: 'UNIX Shell',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/e/master/topics/scm/01-git',
+    text: 'git',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://git-scm.com/download/win',
+    text: 'Git bash',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://help.github.com/articles/fork-a-repo/src',
+    text: 'fork',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://help.github.com/articles/cloning-a-repository/',
+    text: 'Clona',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://nodejs.org/',
+    text: 'Node.js',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
+  },
+];
+
+const linksValidated = [
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://atom.io/',
+    text: 'Atom',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://code.visualstudio.com/',
+    text: 'Code',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/tree/master/topics/shell',
+    text: 'UNIX Shell',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://github.com/Laboratoria/bootcamp/e/master/topics/scm/01-git',
+    text: 'git',
+    status: 404,
+    statusText: 'Fail',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://git-scm.com/download/win',
+    text: 'Git bash',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://help.github.com/articles/fork-a-repo/src',
+    text: 'fork',
+    status: 404,
+    statusText: 'Fail',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://help.github.com/articles/cloning-a-repository/',
+    text: 'Clona',
+    status: 200,
+    statusText: 'Ok',
+  },
+  {
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://nodejs.org/',
     text: 'Node.js',
     status: 200,
     statusText: 'Ok',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://developers.google.com/v8/',
-    text: 'motor de JavaScript V8 de Chrome',
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
     status: 200,
     statusText: 'Ok',
   },
   {
-    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc1/md2.md',
-    href: 'https://nodejs.org/es/ths',
-    text: 'Link',
+    file: '/home/ghlena/Desktop/laboratoria/LIM012-fe-md-links/test/direc/file.md',
+    href: 'https://docs.npmjs.com/',
+    text: 'npm',
     status: 200,
     statusText: 'Ok',
   },
-];
+]; */
 module.exports = { links, linksValidated };
